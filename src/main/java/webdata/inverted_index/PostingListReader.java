67615cf1305245f1221bd061a2ab04414ffc63f3
Daniel Kerbel <nmdanny@gmail.com>
2021-04-16 01:54:53 +0300

    PostingListReader - some doc and remove unused function

--- src/main/java/webdata/inverted_index/PostingListReader.java
+++ src/main/java/webdata/inverted_index/PostingListReader.java
@@ -6,44 +6,16 @@ import java.io.*;
 import java.nio.channels.Channels;
 import java.util.Enumeration;
 import java.util.NoSuchElementException;
-import java.util.stream.IntStream;
 
+/** Allows random access reads of an inverted index file, reading
+ *  a posting list at any valid posting pointer.
+ * */
 public class PostingListReader {
 
     private final String filePath;
 
     public PostingListReader(String path) throws IOException {
         this.filePath = path;
-    }
-
-    /** Returns the posting list at given pointer as a stream of integers, without caring about what they convey,
-     *  and without translating gaps to docIds. */
-    public IntStream readIntegers(int postingPtr, int frequency) throws IOException {
-        assert frequency > 0;
-
-        var raf = new RandomAccessFile(filePath, "r");
-        var fileChannel = raf.getChannel();
-        fileChannel.position(postingPtr);
-        var stream = new BufferedInputStream(Channels.newInputStream(fileChannel));
-        final var decoder = new GroupVarintDecoder(stream);
-
-        return IntStream.iterate(1, num -> num > 0, _prev -> {
-            try {
-                return decoder.read();
-
-            } catch (IOException e) {
-                System.err.format("Error while reading integers at ptr %d: %s", postingPtr, e);
-                return -1;
-            }
-        }).skip(1).limit(frequency).onClose(() -> {
-            try {
-                stream.close();
-                fileChannel.close();
-                raf.close();
-            } catch (IOException e) {
-                System.err.format("IOException while closing resources: %s", e);
-            }
-        });
     }
 
     /** Returns the posting list as a stream of docIds and frequencies */
