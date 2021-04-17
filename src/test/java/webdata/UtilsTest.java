ff17528b03b7e04206546da8a14eddaa1ce77104
Daniel Kerbel <nmdanny@gmail.com>
2021-04-18 02:48:50 +0300

    Implement interleave function

--- src/test/java/webdata/UtilsTest.java
+++ src/test/java/webdata/UtilsTest.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,5 +32,20 @@ class UtilsTest {
         minimals = Utils.getMinElements(heap).collect(Collectors.toList());
         assertTrue(minimals.isEmpty());
 
+    }
+
+    @Test
+    void interleave() {
+        var streams = List.of(
+                List.of(0, 4, 8).spliterator(),
+                List.of(1, 5, 9).spliterator(),
+                List.of(2, 6, 10).spliterator(),
+                List.of(3, 7).spliterator()
+        );
+        var interleavedSplit  = Utils.interleave(streams);
+        var interleaved = StreamSupport.stream(interleavedSplit, false)
+                .collect(Collectors.toList());
+
+        assertIterableEquals(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10), interleaved);
     }
 }
